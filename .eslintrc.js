5d37f246a1e20620101a67a780ccc5a726b3bff0
Joaquin Vilchez <joaquinvilche95@gmail.com>
2024-08-15 11:41:34 -0300

    UPDATE: Basic eslint and prettier configuration and basic pages structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,15 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'next/core-web-vitals',
   ],
   root: true,
   env: {
+    browser: true,
     node: true,
     jest: true,
   },
@@ -21,5 +23,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-unused-vars': 'off',
   },
 };
