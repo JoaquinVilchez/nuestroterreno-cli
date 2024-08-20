d95697151d640a7700b166b90c724eda0e1f16bb
Joaquin Vilchez <joaquinvilche95@gmail.com>
2024-08-20 13:48:22 -0300

    Estructura básica establecida: Configuración de páginas, layout, y solución de problemas con Client/Server Components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,10 @@
 import { Box, Heading } from '@chakra-ui/react';
 
-export default function PublicPage() {
+export default function AdminPage() {
   return (
     <Box padding="4" bg="gray.100" maxW="3xl" mx="auto">
       <Heading as="h1" size="2xl" textAlign="center" mt="8">
-        PUBLIC PAGE
+        ADMIN PAGE
       </Heading>
     </Box>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import { Providers } from './providers';
+'use client';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import theme from '@/theme';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
     </html>
   );
