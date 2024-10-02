b0782017f4eb4b083a0a6fdad6f14c09898c09ed
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2024-10-02 11:45:36 -0300

    Se agrega favicon y titulo de pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,17 @@ import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 import Providers from './providers';
 
+export const metadata = {
+  title: 'Nuestro Terreno 2024 - MVT',
+  icons: {
+    icon: '/favicon.ico', // Ruta a tu favicon
+  },
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <html lang="en">
+      <html lang="es">
         <body>
           <Providers>{children}</Providers>
         </body>
